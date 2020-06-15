1e65d47fcadad34234d78b2ab04e27c2fa5e2762
xemnikk <xemnikk@gmail.com>
2020-06-15 11:45:09 -0500

    added start screen and options

--- lib/Main.rb
+++ lib/Main.rb
@@ -6,4 +6,7 @@ puts "
 ███████║╚██████╔╝██║  ██║╚██████╗███████╗██║  ██║███████╗██║  ██║
 ╚══════╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚══════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝".colorize(:blue)
 
-                      
+prompt = TTY::Prompt.new
+prompt.select("Choose your destiny?", %w(NewGame LoadSave ViewChoices Exit))
+prompt.ask('What is your chosen name?', default: ENV['USER'])
+prompt.mask("What is your password")
