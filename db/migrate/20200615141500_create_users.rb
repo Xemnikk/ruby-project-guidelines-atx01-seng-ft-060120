1e05758b96bba41c4b96d1611d8c2015692fd816
xemnikk <xemnikk@gmail.com>
2020-06-15 15:13:16 -0500

    added options for menu that progress forward."

--- db/migrate/20200615141500_create_users.rb
+++ db/migrate/20200615141500_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :display_name
       t.string :username
       t.string :password
       t.integer :saves_id
