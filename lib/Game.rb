8b4f59e8e52f0e32f2188a9aced5d3646759e57b
VenkatNan <nanditavenkat@gmail.com>
2020-06-17 10:04:43 -0500

    Game hash

--- lib/Game.rb
+++ lib/Game.rb
@@ -3,4 +3,18 @@ class Game < ActiveRecord::Base
     has_many :saves
     has_many :users, through: :saves
 
+    def game_hash
+        
+            Start_game = { 
+                :LeftBranch =>{:LB=>[{ ZeroLB:"", OneLb:"", TwoLB:"",ThreeLB:"",FourLB:"",FiveLB:"",SixLB:"",EndR:""}]},
+                :LCon=>[]
+                },
+
+                :MiddleBranch =>{},
+
+                :RightBranch =>{:RB=>[{ ZeroRB:"", OneRb:"", TwoRB:"",ThreeRB:"",FourRB:"",FiveRB:"",SixRB:"",EndR:""}]},
+                :RCon=>[]}
+            }
+        
+
 end
